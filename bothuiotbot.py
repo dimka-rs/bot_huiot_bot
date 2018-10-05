f1d6626116ea4838b7d088028688cc37e0c6106e
Dmitry Koroban <dmitry@radiofid.ru>
2018-10-05 18:07:14 +0300

    added logging

--- bothuiotbot.py
+++ bothuiotbot.py
@@ -5,10 +5,24 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import sys
 import os
+import logging
 
 TOKEN_FILE="./TOKEN"
-
+LOG_FILE="./bot.log"
 vovels = 'аеийоуэя'
+
+#logging.basicConfig(filename=LOG_FILE,level=logging.DEBUG)
+logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+rootLogger = logging.getLogger()
+rootLogger.setLevel(logging.INFO)
+
+fileHandler = logging.FileHandler(LOG_FILE)
+fileHandler.setFormatter(logFormatter)
+rootLogger.addHandler(fileHandler)
+
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(logFormatter)
+rootLogger.addHandler(consoleHandler)
 
 exists = os.path.isfile(TOKEN_FILE)
 if exists:
@@ -22,17 +36,26 @@ if not len(mytoken):
     print("Failed to read token from %s" % TOKEN_FILE)
     sys.exit(1)
 else:
-    #print("My token: %s" % mytoken)
-    print("Token OK")
+    logging.info('Token OK')
 
 updater = Updater(token=mytoken) 
 dispatcher = updater.dispatcher
 
+def doLog(bot, update):
+    logging.info(
+            "CHAT: " + str(update.message.chat_id)
+        + ". USER: " + str(update.message.from_user)
+        + ". TEXT: " + str(update.message.text)
+        )
 
 def startCommand(bot, update):
+    doLog(bot, update)
     bot.send_message(chat_id=update.message.chat_id, text='You shall not pass, ' + update.message.from_user)
 
+
 def textMessage(bot, update):
+    doLog(bot, update)
+    response = 'GTFO'
     lastword = update.message.text.split(' ')[-1]
     for c in lastword:
         found = vovels.find(c)
